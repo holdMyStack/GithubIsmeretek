c48191951da6b40eb9d45e70e2341c83ff1c0abe
Fata Dávid <fata.david@petrik.hu>
2026-01-29 13:33:27 +0100

    Masodik osztály kész

---
+++ GitGitGitGitGitGitGit/Masodik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGitGitGitGitGitGit
+{
+	internal class Masodik
+	{
+		List<Adat> list;
+
+		public Masodik(List<Adat> list)
+		{
+			this.list = list;
+		}
+
+
+	}
+}
