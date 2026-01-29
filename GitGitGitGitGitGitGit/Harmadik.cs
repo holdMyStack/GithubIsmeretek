3a82ced1b0236aef3fe252acf14a7b2f5a67c4be
Petrik Diak <diak@petrik.hu>
2026-01-29 13:47:14 +0100

    Harmadik osztály kész

---
+++ GitGitGitGitGitGitGit/Harmadik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGitGitGitGitGitGit
+{
+	internal class Harmadik
+	{
+		Dictionary<Adat, int> stat;
+
+		public Harmadik(Dictionary<Adat, int> stat)
+		{
+			this.stat = stat;
+		}
+	}
+}
